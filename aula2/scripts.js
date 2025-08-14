146755eb4819fe282ffa819ddc82020a79debb51
Maria Moreira <maria.ecr.moreira1@gmail.com>
2025-08-14 00:32:14 -0300

    feat: adicionando codigos da aula 1 refente as tecnologias front-end

---
+++ aula2/scripts.js
@@ -0,0 +1,5 @@
+function exec1() {//não esquecer dos () afinal é uma função 
+    let nome = "João";
+    let idade = 25;
+    document.getElementById("resultado1").innerHTML = `Nome: ${nome}, Idade: ${idade}`;
+}
